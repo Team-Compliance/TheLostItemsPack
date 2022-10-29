60c1518bc51866567ccb64e94fc23f432da5b1db
thicco-catto <cristobal.martin01@gmail.com>
2022-10-29 11:29:24 +0200

    forgor to uncomment code

--- lost_items_scripts/BlockDisabledItems.lua
+++ lost_items_scripts/BlockDisabledItems.lua
@@ -13,23 +13,32 @@ end
 LostItemsPack:AddCallback(ModCallbacks.MC_POST_GAME_STARTED, BlockDisabledItems.OnGameStart)
 
 
--- function BlockDisabledItems:PostGetCollectible(selectedItem, poolType, decrease, seed)
---     local isDisabledItem = false
+local isDisablingItem = false
 
---     for _, disabledItem in ipairs(LostItemsPack.RunPersistentData.DisabledItems) do
---         if selectedItem == disabledItem then
---             isDisabledItem = true
---             break
---         end
---     end
+function BlockDisabledItems:PostGetCollectible(selectedItem, poolType, decrease, seed)
+    if isDisablingItem then return end
 
---     if not isDisabledItem then return end
+    local isDisabledItem = false
 
---     local itemPool = Game():GetItemPool()
+    for _, disabledItem in ipairs(LostItemsPack.RunPersistentData.DisabledItems) do
+        if selectedItem == disabledItem then
+            isDisabledItem = true
+            break
+        end
+    end
 
---     local rng = RNG()
---     rng:SetSeed(seed, 35)
+    if not isDisabledItem then return end
 
---     return itemPool:GetCollectible(poolType, decrease, rng:Next())
--- end
--- LostItemsPack:AddCallback(ModCallbacks.MC_POST_GET_COLLECTIBLE, BlockDisabledItems.PostGetCollectible)
+
+    local itemPool = Game():GetItemPool()
+
+    local rng = RNG()
+    rng:SetSeed(seed, 35)
+
+    isDisablingItem = true
+    local newItem = itemPool:GetCollectible(poolType, decrease, rng:Next() * 1000)
+    isDisablingItem = false
+
+    return newItem
+end
+LostItemsPack:AddCallback(ModCallbacks.MC_POST_GET_COLLECTIBLE, BlockDisabledItems.PostGetCollectible)
