36d291e72c94b39a1ef12eea86a0d8e6634c3487
thicco-catto <cristobal.martin01@gmail.com>
2022-11-01 11:25:34 +0100

    feat: Added russian item names

--- lost_items_scripts/Translations.lua
+++ lost_items_scripts/Translations.lua
@@ -13,6 +13,20 @@ local itemTranslations = {
         [LostItemsPack.CollectibleType.PILL_CRUSHER] = {"Triturador de píldoras", "¡Dáselas a todos!"},
         [LostItemsPack.CollectibleType.SAFETY_BOMBS] = {"Bombas de seguridad", "Es por tu propio bien"},
         [LostItemsPack.CollectibleType.VOODOO_PIN] = {"Pin de vudú", "¡Au!"}
+    },
+
+    ru = {
+        [LostItemsPack.CollectibleType.ANCIENT_REVELATION] = {"Древнее откровение", "Recuerda lo que solía haber"},
+        [LostItemsPack.CollectibleType.BETHS_HEART] = {"Сердце Вифании", "Acumulador de fe"},
+        [LostItemsPack.CollectibleType.BLANK_BOMBS] = {"Пустые бомбы", "Entra al sótano"},
+        [LostItemsPack.CollectibleType.BOOK_OF_ILLUSIONS] = {"Книга иллюзий", "Un ejército de ti"},
+        [LostItemsPack.CollectibleType.CHECKED_MATE] = {"Checked Mate", "Amigo ajedrezado"},
+        [LostItemsPack.CollectibleType.KEEPERS_ROPE] = {"Веревка Хранителя", "¡Sácales todo el dinero!"},
+        [LostItemsPack.CollectibleType.LUCKY_SEVEN] = {"Счастливая семерка", "La suerte favorece a la audacia"},
+        [LostItemsPack.CollectibleType.PACIFIST] = {"Пацифист", "Haz el amor, no la guerra"},
+        [LostItemsPack.CollectibleType.PILL_CRUSHER] = {"Дробилка пилюль", "¡Dáselas a todos!"},
+        [LostItemsPack.CollectibleType.SAFETY_BOMBS] = {"Безопасные бомбы", "Es por tu propio bien"},
+        [LostItemsPack.CollectibleType.VOODOO_PIN] = {"Вуду булавка", "¡Au!"}
     }
 }
 
